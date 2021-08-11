0968ef3645e328c6d0c1c3bdb850c53e03276732
scottydphillips <scotty.d.phillips@gmail.com>
2021-08-11 14:35:15 -0400

    server up and running

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
 	useNewUrlParser: true,
 	useFineAndModify: false
 });
 
-app.use(require('api.js'));
+app.use(require('./routes/api.js'));
 
 app.listen(PORT, () => {
 	console.log(`In the words of Frasier Crane, "I'm listening"`)
